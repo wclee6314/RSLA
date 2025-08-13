e6d4f7e4a153432cf4f6a7e4131c148b3e2c2230
wclee6314 <wclee@dnotitia.com>
2025-08-13 01:47:04 +0000

    add turborag

---
+++ scripts/LLM_study_1.py
@@ -0,0 +1,8 @@
+from transformers import pipeline
+
+pipeline = pipeline(task="text-generation", model="Qwen/Qwen2.5-1.5B")
+pipeline("the secret to baking a really good cake is ")
+[{'generated_text': 'the secret to baking a really good cake is 1) to use the right ingredients and 2) to follow the recipe exactly. the recipe for the cake is as follows: 1 cup of sugar, 1 cup of flour, 1 cup of milk, 1 cup of butter, 1 cup of eggs, 1 cup of chocolate chips. if you want to make 2 cakes, how much sugar do you need? To make 2 cakes, you will need 2 cups of sugar.'}]
+
+
+git submodule add https://github.com/MooreThreads/TurboRAG.git third_party/TurboRAG
